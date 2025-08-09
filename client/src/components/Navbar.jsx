36e68d098599810f1af3253f91eb656fc140df49
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-08-10 03:15:42 +0500

    Working on Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets.js'
 
 const Navbar = () => {
   return (
     <div className='fixed top-0 left-0 right-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
-       <Link to={'/'}>
-
+       <Link className='max-md:flex-1' to={'/'}>
+            <img src={assets.logo} className='w-36 h-auto' alt="" />
        </Link>
     </div>
   )
