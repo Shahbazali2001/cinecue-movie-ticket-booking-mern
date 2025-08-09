d784daecaae503fec2d13b827be379234e636940
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-08-10 02:55:37 +0500

    Defining Components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='fixed top-0 left-0 right-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
+       <Link to={'/'}>
+
+       </Link>
+    </div>
+  )
+}
+
+export default Navbar
