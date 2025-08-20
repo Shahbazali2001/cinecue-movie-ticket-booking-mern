05acd48103a44f099ce768a90ad5518d01c83034
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-08-20 05:29:50 +0500

    working on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailersSection from '../components/TrailersSection'
+
+
 
 const Home = () => {
   return (
     <>
         <HeroSection />
+        <FeaturedSection />
+        <TrailersSection />
+        
     </>
   )
 }
